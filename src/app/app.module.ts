393710d695dda137625c8b5c78d18bae5ff601d3
Tim Acker <tjacker31@gmail.com>
2019-09-17 17:53:11 -0400

    Adds navbar component and event thumbnail styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent],
+  declarations: [EventsAppComponent, NavBarComponent, EventsListComponent, EventThumbnailComponent],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
