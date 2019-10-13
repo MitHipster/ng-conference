8d14db461355b87947e85f1b1594b23b1226e011
Tim Acker <tjacker31@gmail.com>
2019-10-13 17:40:21 -0400

    Adds ActivatedRoute service to events detail components to access ID parameter

--- src/app/events/events-detail/events-detail.component.ts
+++ src/app/events/events-detail/events-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { EventService } from '../shared/event.service';
 
 @Component({
@@ -9,9 +11,10 @@ import { EventService } from '../shared/event.service';
 export class EventsDetailComponent implements OnInit {
   event: any;
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    // Plus sign before this.route casts value to a number
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
   }
 }
