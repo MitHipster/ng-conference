9b4bebd0e148a348cd3bc39762f4b1d4381231b2
Tim Acker <tjacker31@gmail.com>
2019-11-03 15:43:17 -0500

    Binds userName, password and submit on login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,7 +4,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'login.component.html'
 })
 export class LoginComponent implements OnInit {
+  userName: string;
+  password: string;
+
   constructor() {}
 
   ngOnInit() {}
+
+  login(formValues) {
+    console.log(formValues);
+  }
 }
