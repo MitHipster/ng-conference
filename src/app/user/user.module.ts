4e3948a2a109f82b49bbe65fc1127f8c3dd18f02
Tim Acker <tjacker31@gmail.com>
2019-11-03 15:28:42 -0500

    Adds login componenet and sets route

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { userRoutes } from './user.routes';
+import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(userRoutes)],
   exports: [],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, LoginComponent]
 })
 export class UserModule {}
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,3 +1,7 @@
+import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 
-export const userRoutes = [{ path: 'profile', component: ProfileComponent }];
+export const userRoutes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent }
+];
